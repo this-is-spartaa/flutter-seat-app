bf0c22a7f415a87144b87825d6cf22d29052bd22
111coding <jwlee1024@gmail.com>
2024-09-29 01:18:15 +0900

    code base

---
+++ lib/seat_select_box.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SeatSelectBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('SeatSelectBox');
+  }
+}
