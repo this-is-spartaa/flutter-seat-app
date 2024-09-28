dc70e7ee3d4dce29104132774efd880b7c10223f
111coding <jwlee1024@gmail.com>
2024-09-29 01:41:14 +0900

    adjust layout

--- lib/seat_bottom.dart
+++ lib/seat_bottom.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class SeatBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('SeatBottom');
+    return Container(
+      color: Colors.blue,
+      height: 200,
+    );
   }
 }
--- lib/seat_select_box.dart
+++ lib/seat_select_box.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class SeatSelectBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('SeatSelectBox');
+    return Expanded(
+      child: Container(
+        color: Colors.green,
+      ),
+    );
   }
 }
