038ba2d1ddea499274ba2fa2650e24d307acb4ac
111coding <jwlee1024@gmail.com>
2024-09-29 03:05:40 +0900

    add SeatBottom

--- lib/seat_bottom.dart
+++ lib/seat_bottom.dart
@@ -4,8 +4,42 @@ class SeatBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      width: double.infinity,
       height: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            '선택된 좌석이 없습니다',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: 200,
+            height: 56,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+              child: Text(
+                'Book now',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
