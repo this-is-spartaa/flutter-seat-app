09e16ba3f3a7831bd031636e630f6bd4a011237c
111coding <jwlee1024@gmail.com>
2024-09-29 02:15:45 +0900

    WIP SeatSelectBox

--- lib/seat_select_box.dart
+++ lib/seat_select_box.dart
@@ -1,11 +1,81 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SeatSelectBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        color: Colors.green,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Screen',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          row(1),
+          SizedBox(height: 10),
+          row(2),
+          SizedBox(height: 10),
+          row(3),
+          SizedBox(height: 10),
+          row(4),
+          SizedBox(height: 10),
+          row(5),
+        ],
+      ),
+    );
+  }
+
+  Padding row(int rowNum) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                '$rowNum',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          seat(),
+          seat(),
+          seat(),
+          seat(),
+          seat(),
+          seat(),
+          seat(),
+          seat(),
+          seat(),
+          seat(),
+        ],
+      ),
+    );
+  }
+
+  Widget seat() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 2),
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
       ),
     );
   }
